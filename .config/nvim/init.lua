4df8f14a6da85444d74cf2747e6a9aad2fdf799e
Zikai Li <lizikai0109@gmail.com>
2024-09-29 16:40:48 -0700

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -643,11 +643,18 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
+    -- opts = {
+    --   transparent = true,
+    --   styles = {
+    --     sidebars = 'transparent',
+    --     floats = 'transparent',
+    --   },
+    -- },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-moon'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
--- .config/sway/lockman.sh
+++ .config/sway/lockman.sh
@@ -3,7 +3,7 @@
 swayidle \
     timeout 15 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
-# Locks the screen immediately
+Locks the screen immediately
 swaylock --screenshots --clock --indicator --indicator-radius 120 --indicator-thickness 7 --effect-blur 7x5
 # Kills last background task so idle timer doesn't keep running
 kill %%
