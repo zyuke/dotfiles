40b3c3acf019c23f39d6f1c3c5bacafc34f581b6
Zikai Li <lizikai0109@gmail.com>
2024-11-11 22:37:52 -0800

    update

--- scripts/eject_wd.sh
+++ scripts/eject_wd.sh
@@ -1,1 +1,1 @@
-sudo udisksctl unmount -b /dev/sda1 && udisksctl unmount -b /dev/sr0 && udisksctl power-off -b /dev/sda1
+sudo udisksctl unmount -b /dev/sdb1 && udisksctl unmount -b /dev/sr0 && udisksctl power-off -b /dev/sdb1
