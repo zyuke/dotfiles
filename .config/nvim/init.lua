f8acb0490f95615caf84c67b19aff2a4ee439f71
Zikai Li <lizikai0109@gmail.com>
2025-08-10 16:40:48 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,10 +2,6 @@
 
 -- neovide settings
 if vim.g.neovide then
-  vim.keymap.set('n', '<C-s>', ':w<CR>') -- Save
-  vim.keymap.set('v', '<C-c>', '"+y') -- Copy
-  vim.keymap.set('n', '<C-v>', '"+P') -- Paste normal mode
-  vim.keymap.set('v', '<C-v>', '"+P') -- Paste visual mode
   vim.keymap.set('c', '<C-v>', '<C-R>+') -- Paste command mode
   vim.keymap.set('i', '<C-v>', '<ESC>l"+Pli') -- Paste insert mode
 end
@@ -32,10 +28,10 @@ tab_keymap('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
 tab_keymap('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
 
 -- Allow clipboard copy paste in neovim
-vim.api.nvim_set_keymap('', '<C-v>', '+p<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('!', '<C-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<C-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<C-v>', '<C-R>+', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('', '<C-v>', '+p<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('!', '<C-v>', '<C-R>+', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('t', '<C-v>', '<C-R>+', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('v', '<C-v>', '<C-R>+', { noremap = true, silent = true })
 
 -- set tab space
 vim.opt['tabstop'] = 4
--- .config/nvim/lua/custom/plugins/env-selector.lua
+++ .config/nvim/lua/custom/plugins/env-selector.lua
@@ -6,15 +6,13 @@ return {
     'mfussenegger/nvim-dap-python', --optional
     { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
   },
-  event = 'VeryLazy',
+  lazy = true,
   branch = 'regexp', -- This is the regexp branch, use this for the new version
-  config = function()
-    require('venv-selector').setup()
-  end,
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
     { '<leader>vs', '<cmd>VenvSelect<cr>' },
-    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+  },
+  opts = {
+    -- Your settings go here
   },
 }
