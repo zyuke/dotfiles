96232766a24ea370c9138b70b7d0e4d6e4ac7ab5
Zikai Li <lizikai0109@gmail.com>
2026-04-26 17:39:19 +0900

    update

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -2,7 +2,7 @@
 return {
   'saghen/blink.cmp',
   event = 'VeryLazy',
-  version = '1.*',
+  version = '*',
   dependencies = {
     -- Snippet Engine
     {
@@ -85,7 +85,7 @@ return {
     -- the rust implementation via `'prefer_rust_with_warning'`
     --
     -- See :h blink-cmp-config-fuzzy for more information
-    fuzzy = { implementation = 'lua' },
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
 
     -- Shows a signature help window while you type arguments for a function
     signature = { enabled = true },
